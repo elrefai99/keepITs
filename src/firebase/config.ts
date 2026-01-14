ad64fcd93a826cac7b5ef311420a302c2ef643f6
elrefai99 <mohamed.mostafa0699@gmail.com>
2026-01-14 14:59:17 +0200

    chore: adding firebase hard code

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 // These values are safe to expose publicly
 const firebaseConfig = {
-     apiKey: "AIzaSyCvLo_ct6hpT-9YtNwsOsl8jQhrG7AG5-k",
-     authDomain: "schedule-5bf3f.firebaseapp.com",
-     projectId: "schedule-5bf3f",
-     storageBucket: "schedule-5bf3f.firebasestorage.app",
-     messagingSenderId: "873645947107",
-     appId: "1:873645947107:web:41f15f2d0f3671c1f26dd6",
-     measurementId: "G-2ZES5ZER8R"
+     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 }
 
 // Initialize Firebase
